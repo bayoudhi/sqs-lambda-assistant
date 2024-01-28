6cf72c49949750c5d06c578e93aa995b199d97bf
Sofienne Lassoued <soflass1293@gmail.com>
2024-01-28 12:45:44 +0100

    update tests

--- src/test/redrive-policy-advisor.test.ts
+++ src/test/redrive-policy-advisor.test.ts
@@ -1,5 +1,5 @@
 import { Worker } from "../core";
-import { VisibilityTimeoutAdvisor } from "../core/visibility-timeout-advisor";
+import { RedrivePolicyAdvisor } from "../core/redrive-policy-advisor";
 
 describe("App", () => {
   it("should contain suggestion 'Consider specifying RedrivePolicy to use a dead letter queue' when no RedrivePolicy present in SQS", () => {
@@ -15,9 +15,9 @@ describe("App", () => {
         },
       },
     });
-    expect(new VisibilityTimeoutAdvisor().apply(worker)).toEqual(
+    expect(new RedrivePolicyAdvisor().apply(worker)).toEqual(
       expect.arrayContaining([
-        "SQS VisibilityTimeout must be greater than Lambda Timeout * 6",
+        "Consider specifying RedrivePolicy to use a dead letter queue",
       ]),
     );
   });
@@ -37,9 +37,14 @@ describe("App", () => {
         },
       },
     });
-    expect(new VisibilityTimeoutAdvisor().apply(worker)).toHaveLength(0);
+    expect(new RedrivePolicyAdvisor().apply(worker)).toHaveLength(1);
+    expect(new RedrivePolicyAdvisor().apply(worker)).toEqual(
+      expect.arrayContaining([
+        "The deadLetterTargetArn must be present in RedrivePolicy",
+      ]),
+    );
   });
-  it("should contain suggestion 'Consider increasing maxReceiveCount' when maxReceiveCount is less than 3", () => {
+  it("should contain suggestion 'Consider increasing maxReceiveCount' when maxReceiveCount is less than 2", () => {
     const worker = new Worker({
       id: "worker",
       lambda: { id: "lambda", Properties: { Timeout: 1 } },
@@ -61,6 +66,9 @@ describe("App", () => {
         },
       },
     });
-    expect(new VisibilityTimeoutAdvisor().apply(worker)).toHaveLength(0);
+    expect(new RedrivePolicyAdvisor().apply(worker)).toHaveLength(1);
+    expect(new RedrivePolicyAdvisor().apply(worker)).toEqual(
+      expect.arrayContaining(["Consider increasing maxReceiveCount"]),
+    );
   });
 });
