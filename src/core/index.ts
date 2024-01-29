8cd950072c8696aa51b9bed59a27cfa9e88b138f
soflass1293 <soflass1293@gmail.com>
2024-01-29 19:05:18 +0100

    feat: Added advisor manager (#11)

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,1 +1,16 @@
+import { AdvisorManager } from "./advisor-manager";
+import { EventSourceMappingStatusAdvisor } from "./event-source-mapping-status-advisor";
+import { MaximumBatchingWindowsAdvisor } from "./maximum-batching-window-advisor";
+import { MessageRetentionPeriodAdvisor } from "./message-retention-period-advisor";
+import { RedrivePolicyAdvisor } from "./redrive-policy-advisor";
+import { VisibilityTimeoutAdvisor } from "./visibility-timeout-advisor";
+
 export * from "./worker";
+
+export const advisor = new AdvisorManager([
+  new EventSourceMappingStatusAdvisor(),
+  new MaximumBatchingWindowsAdvisor(),
+  new MessageRetentionPeriodAdvisor(),
+  new RedrivePolicyAdvisor(),
+  new VisibilityTimeoutAdvisor(),
+]);
